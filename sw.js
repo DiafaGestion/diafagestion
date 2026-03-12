e6e847d8c9d5f32f24b1f0ae8ec0c327230b6091
DiafaGestion <diafaconsulting@gmail.com>
2026-03-12 23:13:36 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,50 +1,14 @@
-const CACHE_NAME = 'diafagestion-v2';
-const ASSETS = [
-  './',
-  './index.html',
-  './manifest.json',
-  './icon-192.png',
-  './icon-512.png',
-  'https://cdnjs.cloudflare.com/ajax/libs/Chart.js/4.4.0/chart.umd.min.js'
-];
+const CACHE_NAME = 'diafagestion-v40';
 
-self.addEventListener('install', e => {
-  e.waitUntil(
-    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
-  );
-  self.skipWaiting();
-});
+self.addEventListener('install', e => { self.skipWaiting(); });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
-    caches.keys().then(keys =>
-      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
-    )
+    caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
 
-// Network first pour index.html (toujours la dernière version), cache pour le reste
 self.addEventListener('fetch', e => {
-  if (e.request.url.includes('index.html') || e.request.url.endsWith('/')) {
-    e.respondWith(
-      fetch(e.request).then(response => {
-        const clone = response.clone();
-        caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
-        return response;
-      }).catch(() => caches.match('./index.html'))
-    );
-  } else {
-    e.respondWith(
-      caches.match(e.request).then(cached => {
-        if (cached) return cached;
-        return fetch(e.request).then(response => {
-          if (!response || response.status !== 200) return response;
-          const clone = response.clone();
-          caches.open(CACHE_NAME).then(cache => cache.put(e.request, clone));
-          return response;
-        }).catch(() => caches.match('./index.html'));
-      })
-    );
-  }
+  e.respondWith(fetch(e.request).catch(() => caches.match('./index.html')));
 });
