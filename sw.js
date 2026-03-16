a9142c1f3f416acc2c3cb4204b1fae1cfba6d35b
DiafaGestion <diafaconsulting@gmail.com>
2026-03-16 23:41:28 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'diafagestion-v117';
+const CACHE_NAME = 'diafagestion-v118';
 const ASSETS = [
   './',
   './index.html',
@@ -17,21 +17,24 @@ const BYPASS_DOMAINS = [
 ];
 
 self.addEventListener('install', e => {
+  // Supprime immédiatement l'ancien SW
+  self.skipWaiting();
   e.waitUntil(
     caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
   );
-  self.skipWaiting();
 });
 
 self.addEventListener('activate', e => {
   e.waitUntil(
     caches.keys().then(keys =>
       Promise.all(
-        keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k))
+        keys.filter(k => k !== CACHE_NAME).map(k => {
+          console.log('[SW] Suppression ancien cache:', k);
+          return caches.delete(k);
+        })
       )
-    )
+    ).then(() => self.clients.claim())
   );
-  self.clients.claim();
 });
 
 self.addEventListener('fetch', e => {
